db9bc4a58b1ef2863b5e6894ef85ae7d49c3758b
nethmidinanjana <nethmidinanjana02@gmail.com>
2023-10-18 09:20:29 +0530

    Updated
    Image Slider

--- app/register/page.js
+++ app/register/page.js
@@ -1,15 +1,15 @@
-const Register = ()=>{
-  return(
+const Register = () => {
+  return (
     <div>
       <section className="flex justify-center w-full min-h-screen">
         <div className="flex frame-container">
-        <div className="flex flex-col items-center justify-center flex-1 bg-orange-500">
+          <div className="flex flex-col items-center justify-center flex-1 bg-orange-500">
 
-</div>
+          </div>
 
-<div className="flex flex-col items-center justify-center flex-1 flex-grow-[1.5] bg-slate-40 bg-slate-400" >
-  
-</div>
+          <div className="flex flex-col items-center justify-center flex-1 flex-grow-[1.5] bg-slate-40 bg-slate-400" >
+
+          </div>
         </div>
       </section>
     </div>
---
+++ components/EventImageSlider.js
@@ -0,0 +1,24 @@
+"use client"
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+import { EffectCards } from 'swiper/modules';
+
+const EventImageSlider = () => {
+  return (
+    <Swiper effect={'cards'} grabCursor={true} modules={[EffectCards]} className="mySwiper">
+      <SwiperSlide></SwiperSlide>
+      <SwiperSlide></SwiperSlide>
+      <SwiperSlide></SwiperSlide>
+      <SwiperSlide></SwiperSlide>
+      <SwiperSlide></SwiperSlide>
+      <SwiperSlide></SwiperSlide>
+      <SwiperSlide></SwiperSlide>
+      <SwiperSlide></SwiperSlide>
+      <SwiperSlide></SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default EventImageSlider;
