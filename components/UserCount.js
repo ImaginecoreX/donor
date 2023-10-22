967100cf7e5d77692220fb12a697ea228ece7328
ByteBigBoss <nethminas42@gmail.com>
2023-10-22 16:42:05 +0530

    Merge changes from main

--- components/UserCount.js
+++ components/UserCount.js
@@ -3,21 +3,21 @@ import React from "react";
 import dornorIcon from "../public/donor.png";
 import donationsIcon from "../public/donations.png";
 
-const UserCount = () => {
+export const UserCount = () => {
   return (
-    <div className="w-full h-auto flex ss:flex-col ns:flex-col ss:gap-3  gap-x-14 mt-6   flex-wrap gap-4">
-      <div className="flex justify-center items-center">
+    <div className="flex flex-wrap w-full h-auto gap-4 mt-6 ss:flex-col ns:flex-col ss:gap-3 gap-x-14">
+      <div className="flex items-center justify-center">
         <div className="flex flex-col gap-4 border-2 rounded-[12px] box-border px-6 py-6 font-semibold shadow-lg ">
-          <div className="flex justify-between gap-11  items-center">
+          <div className="flex items-center justify-between gap-11">
             <div className="">
               <Image src={dornorIcon} className="h-[50px] w-[50px] " />
             </div>
             <div className="text-end">
-              <label className="text-blue-600 text-5xl font-bold font-family-Roboto">
+              <label className="text-5xl font-bold text-blue-600 font-family-Roboto">
                 2000
               </label>
               <br />
-              <label className="text-black text-2xl font-semibold font-family-Raleway">
+              <label className="text-2xl font-semibold text-black font-family-Raleway">
                 Total Donors
               </label>
             </div>
@@ -26,16 +26,16 @@ const UserCount = () => {
       </div>
       <div>
         <div className="flex flex-col gap-4 border-2 rounded-[12px] box-border px-6 py-6 font-semibold shadow-lg">
-          <div className="flex justify-between gap-11 items-center ">
+          <div className="flex items-center justify-between gap-11 ">
             <div className="">
               <Image src={donationsIcon} className="h-[50px] w-[50px] " />
             </div>
             <div className="text-end">
-              <label className="text-blue-600 text-5xl font-bold font-family-Roboto">
+              <label className="text-5xl font-bold text-blue-600 font-family-Roboto">
                 167800$
               </label>
               <br />
-              <label className="text-black text-2xl font-semibold font-family-Raleway">
+              <label className="text-2xl font-semibold text-black font-family-Raleway">
                 Total Donations
               </label>
             </div>
@@ -44,12 +44,12 @@ const UserCount = () => {
       </div>
       <div>
         <div className="flex flex-col gap-4 border-2 rounded-[12px] box-border px-6 py-6 font-semibold shadow-lg">
-          <div className=" flex flex-col items-center ">
-            <label className="text-blue-600 text-5xl font-bold font-family-Roboto">
+          <div className="flex flex-col items-center ">
+            <label className="text-5xl font-bold text-blue-600 font-family-Roboto">
               56
             </label>
         
-            <label className="text-black text-2xl font-semibold font-family-Raleway">
+            <label className="text-2xl font-semibold text-black font-family-Raleway">
               All Request
             </label>
           </div>
@@ -58,12 +58,12 @@ const UserCount = () => {
 
       <div>
         <div className="flex flex-col gap-4 border-2 rounded-[12px] box-border px-6 py-6 font-semibold shadow-lg">
-          <div className=" flex flex-col items-center ">
-            <label className="text-blue-600 text-5xl font-bold font-family-Roboto">
+          <div className="flex flex-col items-center ">
+            <label className="text-5xl font-bold text-blue-600 font-family-Roboto">
               565
             </label>
         
-            <label className="text-black text-2xl font-semibold font-family-Raleway">
+            <label className="text-2xl font-semibold text-black font-family-Raleway">
               All Users
             </label>
           </div>
@@ -74,26 +74,21 @@ const UserCount = () => {
   );
 };
 
-export default UserCount;
-=======
-import React from 'react'
 
-const UserCount = () => {
-    return (
-        <div className='w-full h-auto flex justify-center items-center mt-10 mb-10'>
-            <div className='flex flex-col gap-4 border-2 rounded-[12px] box-border px-6 py-6 font-semibold shadow-lg'>
-                <div className='flex justify-between gap-5'>
-                    <div className='rounded-full bg-blue-400 flex justify-center items-center w-[100px] h-[100px]'>
-                        <span className='text-4xl font-family-Roboto-Serif '>2K</span>
-                    </div>
-                    <div className='flex flex-col justify-center '>
-                        <label className='text-3xl font-family-Raleway color-yellow-500'>Registered</label>
-                        <label className='text-2xl  font-family-Raleway color-blue-500'>Users</label>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
-
-export default UserCount
+// const UserCount = () => {
+//     return (
+//         <div className='flex items-center justify-center w-full h-auto mt-10 mb-10'>
+//             <div className='flex flex-col gap-4 border-2 rounded-[12px] box-border px-6 py-6 font-semibold shadow-lg'>
+//                 <div className='flex justify-between gap-5'>
+//                     <div className='rounded-full bg-blue-400 flex justify-center items-center w-[100px] h-[100px]'>
+//                         <span className='text-4xl font-family-Roboto-Serif '>2K</span>
+//                     </div>
+//                     <div className='flex flex-col justify-center '>
+//                         <label className='text-3xl font-family-Raleway color-yellow-500'>Registered</label>
+//                         <label className='text-2xl font-family-Raleway color-blue-500'>Users</label>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
