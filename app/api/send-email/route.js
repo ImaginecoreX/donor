8ce2d6af91e797eb46061010cc10c225ff1297da
Nethmina Sandaruwan <125788788+ByteBigBoss@users.noreply.github.com>
2023-10-28 02:35:34 +0530

    dev-{update email sending fun}-{2023-10-28}

--- app/api/send-email/route.js
+++ app/api/send-email/route.js
@@ -9,24 +9,24 @@ export async function POST (request) {
     const emailData = await request.json();
 
       const oauth2Client = new OAuth2(
-        '870284985778-gphacdg741uboloh096gidrmb4vo4huq.apps.googleusercontent.com',//Client ID
-        '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',//Client Secret
+        '870284985778-j63j9a8u553ug1l6bocfvkphlr1t6de3.apps.googleusercontent.com',//Client ID
+        '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',//Client Secret
         'https://developers.google.com/oauthplayground',// Redirect URL for testing
       );
   
 
       oauth2Client.setCredentials({
-        refresh_token:'1//04pDzPdlkfowqCgYIARAAGAQSNwF-L9Ir9d5oCE9ujkCQCsESJIVlAL-YMlwAXJ2Wnu692IFOyVsTEaElowc_2WVV-D-tvRn6Xdw',
+        refresh_token:'1//04HTQ3Vy2AlipCgYIARAAGAQSNwF-L9Irs_VBFcNnWJZ1R_FuFnoYtJ1y6Aph_2gKamgAXNJa47Tl0ggvxeDy-51erBAgwS3wM-Q',
       });
   
       const transporter = nodemailer.createTransport({
         service:'Gmail',
         auth:{
           type: 'OAuth2',
-          user: 'nethminas42@gmail.com',
-          clientId:'870284985778-gphacdg741uboloh096gidrmb4vo4huq.apps.googleusercontent.com',
-          clientSecret:'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',
-          refreshToken:'1//04pDzPdlkfowqCgYIARAAGAQSNwF-L9Ir9d5oCE9ujkCQCsESJIVlAL-YMlwAXJ2Wnu692IFOyVsTEaElowc_2WVV-D-tvRn6Xdw',
+          user: 'info.givedonor@gmail.com',
+          clientId:'870284985778-j63j9a8u553ug1l6bocfvkphlr1t6de3.apps.googleusercontent.com',
+          clientSecret:'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',
+          refreshToken:'1//04HTQ3Vy2AlipCgYIARAAGAQSNwF-L9Irs_VBFcNnWJZ1R_FuFnoYtJ1y6Aph_2gKamgAXNJa47Tl0ggvxeDy-51erBAgwS3wM-Q',
           accessToken: oauth2Client.getAccessToken(),
         },
       });
@@ -36,7 +36,7 @@ export async function POST (request) {
       try {
         // Send mail with defined transport object
         await transporter.sendMail({
-          from:'nethminas42@gmail.com',
+          from:'info.givedonor@gmail.com',
           to:emailData.to,
           subject:emailData.subject,
           text:emailData.text,
